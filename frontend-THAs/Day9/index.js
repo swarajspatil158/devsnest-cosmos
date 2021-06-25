b02f5d182c6306bb890fa177cdcad8b569da0cdf
Swaraj Patil <swarajspatil158@gmail.com>
2021-06-25 18:38:33 +0530

    Day9 THA updated

--- frontend-THAs/Day9/index.js
+++ frontend-THAs/Day9/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 var mainBox = document.getElementById("main");
-var count = 400;
+var count = 25;
 var booked = 0;
 document.getElementById("total-seats").innerText = `Total Seats: ${count}`;
 document.getElementById("booked-seats").innerText = `Booked Seats: ${booked}`;
 
-for (let i = 0; i < 400; i++) {
+for (let i = 0; i < 36; i++) {
     let gridDiv = document.createElement("div");
-    gridDiv.style.width = "40px";
-    gridDiv.style.height = "40px";
+    gridDiv.style.width = "130px";
+    gridDiv.style.height = "90px";
     gridDiv.classList.add("red");
     gridDiv.addEventListener("click", () => {
     if (gridDiv.className === "red") {
