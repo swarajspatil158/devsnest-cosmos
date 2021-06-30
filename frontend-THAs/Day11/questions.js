765b7d2b578153aa758ef3400186730bc0e27d1b
Swaraj Patil <swarajspatil158@gmail.com>
2021-06-30 12:33:51 +0530

    Day11 THA Added

---
+++ frontend-THAs/Day11/questions.js
@@ -0,0 +1,8 @@
+var questions = [
+  ["Who is Peter Parker’s nemesis in the first Spider-Man film?", "The Joker", "Magneto", "The Green Goblin","None", "3"],
+  ["What is the name of the cosmic cube that Loki takes control of in The Avengers?", "the Tesseract", "the Magic Cube", "the Mjolnir", "all","1"],
+  [" How many Avengers are there in the Avengers movie?", " 4", " 10", "6", "100","3"],
+  [" What’s the name of Tony Stark’s company in the Iron Man trilogy?", "  Stark and Sons", "  Stark and Co", " Stark Industries", "Avg co","3"],
+  ["How many years was Captain America frozen for in Captain America: The First Avenger?", " Nearly 100 years", " Nearly 70 years", "Nearly 20 years", "Nearly 10 years","2"],
+  ["Who is Thor’s main romantic interest in Thor?", "Jane Foster", "Jean Grey", "Mary Jane", "peta Jenson","1"],
+  ];
