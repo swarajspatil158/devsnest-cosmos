fb1e7346a8df414f3bf17a29c549abcccfcf0286
Swaraj Patil <swarajspatil158@gmail.com>
2021-07-02 22:29:45 +0530

    Day 15 THA Added

---
+++ frontend-THAs/Day15/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./myComponents/Header";
+import Hello from "./myComponents/Hello";
+
+function App() {
+  return (
+ <>
+ <Header/>
+ <Hello/>
+ </>
+  );
+}
+
+export default App;
