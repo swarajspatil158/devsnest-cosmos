a7755bb1225c13f40e602f188e66feb0c5d7f71b
Swaraj Patil <swarajspatil158@gmail.com>
2021-07-21 19:23:54 +0530

    Day20 THA completed

---
+++ frontend-THAs/day20/src/List.js
@@ -0,0 +1,8 @@
+export default [
+    {"foodName":"Pizza", "foodType":"junk", "protein":"chicken", "calories":55},
+    {"foodName":"Burger", "foodType":"junk", "protein":"chicken", "calories":100},
+    {"foodName":"Roti", "foodType":"desi", "protein":"somewhere", "calories":59},
+    {"foodName":"Dahi Vada", "foodType":"desi", "protein":"somewhere", "calories":37},
+    {"foodName":"Samosa", "foodType":"desi", "calories":63},
+    {"foodName":"Jalebi", "foodType":"sweet", "calories":44}
+  ]
