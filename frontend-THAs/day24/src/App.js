6dda4deeca7f5350bea5f94d0dfff482d8d8652c
Swaraj Patil <swarajspatil158@gmail.com>
2021-08-06 16:28:03 +0530

    day 24 THA added

--- frontend-THAs/day24/src/App.js
+++ frontend-THAs/day24/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { div } from "prelude-ls";
+import React,{useState} from "react";
+import LoginForm from "./components/LoginForm";
+
+
 
 function App() {
+  const adminUser ={
+    email: "admin@admin.com",
+    password: "admin"
+
+  }
+
+  const [user,setUser]= useState({name:"", email: ""})
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+    
+    if(details.email == adminUser.email && details.password == adminUser.password)
+    {console.log("Logged in");
+    setUser({
+      name:details.name,
+      email:details.email
+    })
+  
+  }
+    else{
+      setError("deatils do not match")
+    }
+  }
+
+
+  const Logout = () => {
+   setUser({name: "", email: ""});
+  }  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           {(user.email != "")?(
+             <div className="welcome">
+               <h2>Welcome, <span>{user.name}</span></h2>
+               <button onClick={Logout}>Logout</button>
+               </div>
+           ):(
+             <LoginForm Login={Login} error = {error}/>
+           )}
     </div>
   );
 }
