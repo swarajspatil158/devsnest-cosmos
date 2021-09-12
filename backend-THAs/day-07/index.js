52c71f833cfdb04912feb1e7cfb0e7d2c80ab9bc
Swaraj Patil <swarajspatil158@gmail.com>
2021-09-12 11:42:22 +0530

    completed

---
+++ backend-THAs/day-07/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// CRUD -> POST GET PUT DELETE
+app.get('/',(req,res)=>{
+  // res.sendStatus(500);
+  res.json({ r:1,b:2});
+  // res.json({ d:1,c:5});
+  res.send()
+});
+
+
+app.listen(5000);
