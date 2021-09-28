2de6c0a0fc35126902c28ee3a077127f936435ee
Swaraj Patil <swarajspatil158@gmail.com>
2021-09-28 15:01:25 +0530

    updated

--- backend-THAs/day-09/index.js
+++ backend-THAs/day-09/index.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/about',(req,res)=>{
-    // res.send('about')
-    // res.sendFile(path.join(__dirname,'index.html'))
-    res.json({"tech":'vim'})
+app.use('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'/public/Hello.txt'),'test.txt')
 })
 
 app.listen(port, () => {
